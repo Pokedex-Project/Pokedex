4cec23774f7f713b68fc1a705dc699b4caf64b48
MichaelMakinde <michael_makinde@outlook.com>
2023-01-17 03:01:07 -0500

    Created API fetch function, displayed pokemon on page

--- app.js
+++ app.js
@@ -1,20 +1,42 @@
-const pokemonURL = 'https://pokeapi.co/api/v2/pokemon/1/';
-async function getPokemonByNum(number) {
-    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${number}`);
-    const data = await res.json();
-    return data;
+// Pokedex JavaScript
+
+const getPokemon = () => {
+    async function getPokemonByNum(number) {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${number}`);
+        const data = await res.json();
+        return data;
+    }
+
+    const pokeDex = [];
+
+    for (let i = 1; i <= 151; i++) {
+        pokeDex.push(getPokemonByNum(i));
+
+    }
+
+    Promise.all(pokeDex)
+        .then((arrayOfPokemon) => {
+            const pokeDex = arrayOfPokemon.map((data) => ({
+                name: data.name,
+                id: data.id,
+                image: data.sprites.other['official-artwork'].front_default,
+                type: data.types.map((type) => type.type.name).join(', '),
+            }));
+            console.log(pokeDex);
+            showPokemon(pokeDex);
+        });
 }
-const pokeBag = [];
 
-for (let i = 1; i <= 151; i++) {
-    pokeBag.push(getPokemonByNum(i));
-}
-console.log(pokeBag);
-
-Promise.all(pokeBag)
-
-    .then((arrayOfPokemon) => {
-        arrayOfPokemon.forEach(pokemon => {
-            console.log(pokemon.name);
-        })
+const showPokemon = (pokeDex) => {
+    const olElement = document.querySelector('ol');
+    pokeDex.forEach((poke) => {
+        const listElement = document.createElement('li');
+        const imageElement = document.createElement('img');
+        imageElement.src = poke.image;
+        imageElement.alt = poke.image.title;
+        listElement.appendChild(imageElement);
+        olElement.appendChild(listElement);
     })
+}
+
+getPokemon();   
