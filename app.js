dc27a97d3dddef50028e40c45c8767bad907658d
MichaelMakinde <michael_makinde@outlook.com>
2023-01-17 06:57:31 -0500

    Search bar functionality partially complete

--- app.js
+++ app.js
@@ -1,35 +1,37 @@
 // Pokedex JavaScript
 
-const getPokemon = () => {
+const pokeDex = {};
+pokeDex.generation = 1;
+pokeDex.numPokemon = 151;
+pokeDex.region = 'Kanto';
+pokeDex.pokemon = [];
+
+pokeDex.getPokemon = () => {
     async function getPokemonByNum(number) {
         const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${number}`);
         const data = await res.json();
         return data;
     }
 
-    const pokeDex = [];
-
     for (let i = 1; i <= 151; i++) {
-        pokeDex.push(getPokemonByNum(i));
-
+        pokeDex.pokemon.push(getPokemonByNum(i));
     }
 
-    Promise.all(pokeDex)
+    Promise.all(pokeDex.pokemon)
         .then((arrayOfPokemon) => {
-            const pokeDex = arrayOfPokemon.map((data) => ({
+            pokeDex.pokemon = arrayOfPokemon.map((data) => ({
                 name: data.name,
                 id: data.id,
                 image: data.sprites.other['official-artwork'].front_default,
                 type: data.types.map((type) => type.type.name).join(', '),
             }));
-            console.log(pokeDex);
-            showPokemon(pokeDex);
+            pokeDex.showPokemon(pokeDex.pokemon);
         });
 }
 
-const showPokemon = (pokeDex) => {
+pokeDex.showPokemon = (pokemon) => {
     const olElement = document.querySelector('ol');
-    pokeDex.forEach((poke) => {
+    pokeDex.pokemon.forEach((poke) => {
         const listElement = document.createElement('li');
         const imageElement = document.createElement('img');
         imageElement.src = poke.image;
@@ -44,4 +46,37 @@ const showPokemon = (pokeDex) => {
     });
 };
 
-getPokemon();   
+pokeDex.searchBar = (pokemon) => {
+    const ulElement = document.querySelector('ul');
+    const listElement = document.createElement('li');
+    const imageElement = document.createElement('img');
+    imageElement.src = pokemon.image;
+    imageElement.alt = `Official Artwork for pokemon ${pokemon.name}`;
+    listElement.appendChild(imageElement);
+    ulElement.appendChild(listElement);
+    listElement.innerHTML = `<p class="pokeId"><span class="idBold">ID:</span> ${pokemon.id}</p>
+    <img src="${imageElement.src}" alt="${imageElement.alt}">
+    <p class="pokeName"><span>Name:</span> ${pokemon.name}</p>
+    <p class="pokeType"><span>Type:</span> ${pokemon.type}</p>`;
+};
+
+
+pokeDex.searchPokemon = () => {
+    const searchInput = document.getElementById('search');
+    searchInput.addEventListener("keyup", (event) => {
+        const value = event.target.value;
+        const showSearch = pokeDex.pokemon.filter((poke) => {
+            return (poke.name == value || poke.id == value);
+        })
+        console.log(showSearch[0]);
+        pokeDex.searchBar(showSearch[0]);
+    });
+};
+
+pokeDex.init = () => {
+    pokeDex.getPokemon();
+    console.log(pokeDex);
+    pokeDex.searchPokemon();
+};
+
+pokeDex.init(); 
